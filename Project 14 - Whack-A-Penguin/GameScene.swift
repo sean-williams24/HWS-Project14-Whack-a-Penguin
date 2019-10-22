3ed18cd0cd4ab0e42d1184958581b2fca9a1f780
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-10-22 11:06:50 +0100

    FEAT: show moles on timed intervals in groups of 1, 2, 3 or 4. Hide them using delayed intervals. Create hit method.

--- Project 14 - Whack-A-Penguin/GameScene.swift
+++ Project 14 - Whack-A-Penguin/GameScene.swift
@@ -18,7 +18,8 @@ class GameScene: SKScene {
             gameScore.text = "Score: \(score)"
         }
     }
-    
+    var popUpTime = 0.85
+
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "whackBackground")
@@ -38,6 +39,10 @@ class GameScene: SKScene {
         for i in 0..<5 {createSlot(at: CGPoint(x: 180 + (i * 170), y: 320)) }
         for i in 0..<5 {createSlot(at: CGPoint(x: 100 + (i * 170), y: 230)) }
         for i in 0..<5 {createSlot(at: CGPoint(x: 180 + (i * 170), y: 140)) }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.createEnemy()
+        }
 
     }
     
@@ -55,5 +60,26 @@ class GameScene: SKScene {
         slot.configure(at: position)
         addChild(slot)
         slots.append(slot)
+    }
+    
+    func createEnemy() {
+        popUpTime *= 0.991
+        
+        slots.shuffle()
+        slots[0].show(hideTime: popUpTime)
+        
+        if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popUpTime)}
+        if Int.random(in: 0...12) > 8 { slots[2].show(hideTime: popUpTime)}
+        if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popUpTime)}
+        if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popUpTime)}
+        
+        let minDelay = popUpTime / 2.0
+        let maxDelay = popUpTime * 2
+        let delay = Double.random(in: minDelay...maxDelay)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+            self?.createEnemy()
+        }
+
     }
 }
