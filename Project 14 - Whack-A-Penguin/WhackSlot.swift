fd82c2b234ef67cdd0624860be5d450eabe7fffc
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-10-22 10:09:44 +0100

    FEAT: Add background image and score label. Create slot method to configure slots using for loops to lineup in rows. add whack slot SKNode with Mask to hide penguin.

---
+++ Project 14 - Whack-A-Penguin/WhackSlot.swift
@@ -0,0 +1,34 @@
+//
+//  WhackSlot.swift
+//  Project 14 - Whack-A-Penguin
+//
+//  Created by Sean Williams on 22/10/2019.
+//  Copyright © 2019 Sean Williams. All rights reserved.
+//
+
+import SpriteKit
+import UIKit
+
+class WhackSlot: SKNode {
+    
+    var charNode: SKSpriteNode!
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+    }
+}
